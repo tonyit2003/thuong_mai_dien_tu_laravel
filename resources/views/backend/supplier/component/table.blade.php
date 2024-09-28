fb1507d40767f394ad0dd96ba760e090fc5796bf
CongCaoIT <103946367+CongCaoIT@users.noreply.github.com>
2024-09-28 22:43:17 +0700

    Sua hien thi danh sach thong tin nha cung cap

--- resources/views/backend/supplier/component/table.blade.php
+++ resources/views/backend/supplier/component/table.blade.php
@@ -19,68 +19,51 @@
             @php
                 $previousSupplierId = null;
             @endphp
-            @foreach ($suppliers as $supplier)
-                @if ($previousSupplierId === $supplier->id)
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>
-                            {{ $supplier->product_type_name ?? __('No Product Types') }}
-                        </td>
-                        <td class="text-center"></td>
-                        <td class="text-center"></td>
-                    </tr>
-                @else
-                    <tr>
-                        <td class="text-center">
-                            <input type="checkbox" value="{{ $supplier->id }}" class="input-checkbox checkBoxItem" />
-                        </td>
-                        <td>{{ $supplier->name }}</td>
-                        <td>{{ $supplier->email }}</td>
-                        <td>{{ $supplier->phone }}</td>
-                        <td>
-                            {{ $supplier->address }},
-                            {{ optional($supplier->ward)->full_name }},
-                            {{ optional($supplier->district)->full_name }},
-                            {{ optional($supplier->province)->full_name }}
-                        </td>
-                        <td class="text-center">{{ $supplier->fax }}</td>
-                        <td>
-                            {{ $supplier->product_type_name ?? __('No Product Types') }}
-                        </td>
-                        <td class="text-center js-switch-{{ $supplier->id }}">
-                            <input type="checkbox" value="{{ $supplier->publish }}" class="js-switch status" data-field="publish"
-                                data-model="{{ $config['model'] }}" data-modelId="{{ $supplier->id }}"
-                                {{ $supplier->publish == 1 ? 'checked' : '' }} />
-                        </td>
-                        <td class="text-center">
-                            <div class="ibox-tools-button">
-                                <a class="dropdown-toggle" data-toggle="dropdown" href="#" style="color: #000">
-                                    <strong style="min-width: 0px">...</strong>
-                                </a>
-                                <ul class="dropdown-menu dropdown-user" style="font-size: 13px; left: -170px">
-                                    <li>
-                                        <a href="{{ route('supplier.edit', $supplier->id) }}">
-                                            {{ __('table.update') }}
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('supplier.delete', $supplier->id) }}">
-                                            {{ __('table.delete') }}
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </td>
-                    </tr>
-                @endif
+            @foreach ($suppliers->groupBy('id') as $supplierGroup)
                 @php
-                    $previousSupplierId = $supplier->id;
+                    $supplier = $supplierGroup->first();
+                    $productTypes = $supplierGroup->pluck('product_type_name')->filter()->unique()->implode(', ');
                 @endphp
+                <tr>
+                    <td class="text-center" rowspan="1">
+                        <input type="checkbox" value="{{ $supplier->id }}" class="input-checkbox checkBoxItem" />
+                    </td>
+                    <td>{{ $supplier->name }}</td>
+                    <td>{{ $supplier->email }}</td>
+                    <td>{{ $supplier->phone }}</td>
+                    <td>
+                        {{ $supplier->address }},
+                        {{ optional($supplier->ward)->full_name }},
+                        {{ optional($supplier->district)->full_name }},
+                        {{ optional($supplier->province)->full_name }}
+                    </td>
+                    <td class="text-center">{{ $supplier->fax }}</td>
+                    <td>{{ $productTypes ?? __('No Product Types') }}</td>
+                    <td class="text-center js-switch-{{ $supplier->id }}">
+                        <input type="checkbox" value="{{ $supplier->publish }}" class="js-switch status" data-field="publish"
+                            data-model="{{ $config['model'] }}" data-modelId="{{ $supplier->id }}"
+                            {{ $supplier->publish == 1 ? 'checked' : '' }} />
+                    </td>
+                    <td class="text-center">
+                        <div class="ibox-tools-button">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#" style="color: #000">
+                                <strong style="min-width: 0px">...</strong>
+                            </a>
+                            <ul class="dropdown-menu dropdown-user" style="font-size: 13px; left: -170px">
+                                <li>
+                                    <a href="{{ route('supplier.edit', $supplier->id) }}">
+                                        {{ __('table.update') }}
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('supplier.delete', $supplier->id) }}">
+                                        {{ __('table.delete') }}
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </td>
+                </tr>
             @endforeach
         @endif
     </tbody>
