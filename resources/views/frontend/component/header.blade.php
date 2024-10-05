c5f6de8d91454a0ab88c31fd1783f99a5c1b8143
CongCaoIT <103946367+CongCaoIT@users.noreply.github.com>
2024-10-05 13:24:46 +0700

    Hoan thien chuc nang dang nhap nguoi dung

--- resources/views/frontend/component/header.blade.php
+++ resources/views/frontend/component/header.blade.php
@@ -8,11 +8,7 @@
                             @foreach ($menus[App\Enums\MenuEnum::SITE_LINK_MENU] as $key => $val)
                                 @php
                                     $name = $val['item']->languages->first()->pivot->name;
-                                    $canonical = write_url(
-                                        $val['item']->languages->first()->pivot->canonical ?? '',
-                                        true,
-                                        true,
-                                    );
+                                    $canonical = write_url($val['item']->languages->first()->pivot->canonical ?? '', true, true);
                                 @endphp
                                 <li><a href="{{ $canonical }}" title="{{ $name }}">{{ $name }}</a></li>
                             @endforeach
@@ -22,8 +18,7 @@
                 <div class="slogan">{{ $system['homepage_slogan'] }}</div>
                 <div class="header-widget">
                     <div class="uk-flex uk-flex-middle">
-                        <div class="call-us">{{ __('homePage.call_me_to_help') }}: <a
-                                href="tel: {{ $system['contact_hotline'] }}"
+                        <div class="call-us">{{ __('homePage.call_me_to_help') }}: <a href="tel: {{ $system['contact_hotline'] }}"
                                 title="">{{ $system['contact_hotline'] }}</a></div>
                         @if (isset($languages))
                             <div class="language">
@@ -44,8 +39,7 @@
                                             }
                                         @endphp
                                         <li>
-                                            <a href="#" title="{{ $val->name }}"
-                                                class="change-language">{{ $val->name }}</a>
+                                            <a href="#" title="{{ $val->name }}" class="change-language">{{ $val->name }}</a>
                                         </li>
                                     @endforeach
                                 </ul>
@@ -68,8 +62,7 @@
                     <div class="header-form">
                         <form action="" class="uk-form form search-form">
                             <div class="form-row">
-                                <input type="text" name="" class="input-text"
-                                    placeholder="Tìm kiếm sản phẩm...">
+                                <input type="text" name="" class="input-text" placeholder="Tìm kiếm sản phẩm...">
                             </div>
                             <button type="submit" name="search" value="submit">Search</button>
                         </form>
@@ -77,14 +70,12 @@
                 </div>
                 <div class="middle-widget">
                     <div class="uk-flex uk-flex-middle">
-                        <a href="" class="become-vendor">Trở thành đại lý <i class="fa fa-long-arrow-right"
-                                aria-hidden="true"></i></a>
+                        <a href="" class="become-vendor">Trở thành đại lý <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
                         <div class="widget-toolbox uk-flex uk-flex-middle">
                             <div class="toolbox-item">
                                 <a href="" title="" class="uk-flex uk-flex-bottom">
                                     <div class="icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"
-                                            viewBox="0 0 26 26" fill="none">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26" fill="none">
                                             <g>
                                                 <path
                                                     d="M20.298 8.38686L21.7149 7.56033C18.6493 2.2939 12.0415 0.282014 6.56113 2.94644V0.930145H4.9212V5.84994H9.84099V4.21001H7.74598C12.3848 2.24224 17.7631 4.03197 20.298 8.38686Z"
@@ -110,8 +101,7 @@
                             <div class="toolbox-item">
                                 <a href="" title="" class="uk-flex uk-flex-bottom">
                                     <div class="icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"
-                                            viewBox="0 0 25 25" fill="none">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
                                             <g>
                                                 <path
                                                     d="M18.2753 1.28351C17.1493 1.30102 16.0478 1.61536 15.0821 2.19478C14.1164 2.7742 13.3207 3.59818 12.7753 4.58351C12.23 3.59818 11.4343 2.7742 10.4686 2.19478C9.50289 1.61536 8.4014 1.30102 7.27535 1.28351C5.48029 1.3615 3.78905 2.14676 2.57113 3.46774C1.35321 4.78872 0.707598 6.53803 0.775349 8.33351C0.775349 15.1085 11.7313 22.9335 12.1973 23.2655L12.7753 23.6745L13.3533 23.2655C13.8193 22.9355 24.7753 15.1085 24.7753 8.33351C24.8431 6.53803 24.1975 4.78872 22.9796 3.46774C21.7616 2.14676 20.0704 1.3615 18.2753 1.28351ZM12.7753 21.2125C9.52235 18.7825 2.77535 12.8125 2.77535 8.33351C2.70699 7.06822 3.14172 5.82724 3.98471 4.88121C4.82771 3.93518 6.01058 3.36086 7.27535 3.28351C8.54012 3.36086 9.72299 3.93518 10.566 4.88121C11.409 5.82724 11.8437 7.06822 11.7753 8.33351H13.7753C13.707 7.06822 14.1417 5.82724 14.9847 4.88121C15.8277 3.93518 17.0106 3.36086 18.2753 3.28351C19.5401 3.36086 20.723 3.93518 21.566 4.88121C22.409 5.82724 22.8437 7.06822 22.7753 8.33351C22.7753 12.8145 16.0283 18.7825 12.7753 21.2125Z"
@@ -119,8 +109,8 @@
                                             </g>
                                             <defs>
                                                 <clipPath>
-                                                    <rect width="24" height="24" fill="white"
-                                                        transform="translate(0.775391 0.366516)"></rect>
+                                                    <rect width="24" height="24" fill="white" transform="translate(0.775391 0.366516)">
+                                                    </rect>
                                                 </clipPath>
                                             </defs>
                                         </svg>
@@ -133,8 +123,7 @@
                             <div class="toolbox-item">
                                 <a href="" title="" class="uk-flex uk-flex-bottom">
                                     <div class="icon">
-                                        <svg width="25" height="25" viewBox="0 0 25 25" fill="none"
-                                            xmlns="http://www.w3.org/2000/svg">
+                                        <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <g>
                                                 <path
                                                     d="M24.4941 3.36652H4.73614L4.69414 3.01552C4.60819 2.28593 4.25753 1.61325 3.70863 1.12499C3.15974 0.636739 2.45077 0.366858 1.71614 0.366516L0.494141 0.366516V2.36652H1.71614C1.96107 2.36655 2.19748 2.45647 2.38051 2.61923C2.56355 2.78199 2.68048 3.00626 2.70914 3.24952L4.29414 16.7175C4.38009 17.4471 4.73076 18.1198 5.27965 18.608C5.82855 19.0963 6.53751 19.3662 7.27214 19.3665H20.4941V17.3665H7.27214C7.02705 17.3665 6.79052 17.2764 6.60747 17.1134C6.42441 16.9505 6.30757 16.7259 6.27914 16.4825L6.14814 15.3665H22.3301L24.4941 3.36652ZM20.6581 13.3665H5.91314L4.97214 5.36652H22.1011L20.6581 13.3665Z"
@@ -148,8 +137,8 @@
                                             </g>
                                             <defs>
                                                 <clipPath>
-                                                    <rect width="24" height="24" fill="white"
-                                                        transform="translate(0.494141 0.366516)"></rect>
+                                                    <rect width="24" height="24" fill="white" transform="translate(0.494141 0.366516)">
+                                                    </rect>
                                                 </clipPath>
                                             </defs>
                                         </svg>
@@ -160,26 +149,34 @@
                                 </a>
                             </div>
                             <div class="toolbox-item">
-                                <a href="" title="" class="uk-flex uk-flex-bottom">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"
-                                        viewBox="0 0 25 25" fill="none">
-                                        <g>
-                                            <path
-                                                d="M21.4443 24.3665H19.4443V19.3235C19.4435 18.5395 19.1317 17.7879 18.5774 17.2335C18.023 16.6791 17.2713 16.3673 16.4873 16.3665H8.40134C7.61733 16.3673 6.86567 16.6791 6.3113 17.2335C5.75693 17.7879 5.44513 18.5395 5.44434 19.3235V24.3665H3.44434V19.3235C3.44592 18.0093 3.96869 16.7494 4.89796 15.8201C5.82723 14.8909 7.08714 14.3681 8.40134 14.3665H16.4873C17.8015 14.3681 19.0614 14.8909 19.9907 15.8201C20.92 16.7494 21.4427 18.0093 21.4443 19.3235V24.3665Z"
-                                                fill="#253D4E"></path>
-                                            <path
-                                                d="M12.4443 12.3665C11.2577 12.3665 10.0976 12.0146 9.11092 11.3553C8.12422 10.696 7.35519 9.75898 6.90106 8.66262C6.44694 7.56626 6.32812 6.35986 6.55963 5.19598C6.79114 4.03209 7.36258 2.96299 8.2017 2.12388C9.04081 1.28476 10.1099 0.713318 11.2738 0.481807C12.4377 0.250296 13.6441 0.369116 14.7404 0.823242C15.8368 1.27737 16.7739 2.0464 17.4332 3.0331C18.0924 4.01979 18.4443 5.17983 18.4443 6.36652C18.4427 7.95733 17.8101 9.48253 16.6852 10.6074C15.5604 11.7323 14.0352 12.3649 12.4443 12.3665ZM12.4443 2.36652C11.6532 2.36652 10.8799 2.60111 10.2221 3.04064C9.56426 3.48017 9.05157 4.10488 8.74882 4.83579C8.44607 5.56669 8.36686 6.37096 8.5212 7.14688C8.67554 7.9228 9.0565 8.63554 9.61591 9.19495C10.1753 9.75436 10.8881 10.1353 11.664 10.2897C12.4399 10.444 13.2442 10.3648 13.9751 10.062C14.706 9.75929 15.3307 9.2466 15.7702 8.5888C16.2097 7.931 16.4443 7.15764 16.4443 6.36652C16.4443 5.30565 16.0229 4.28824 15.2728 3.53809C14.5226 2.78795 13.5052 2.36652 12.4443 2.36652Z"
-                                                fill="#253D4E"></path>
-                                        </g>
-                                        <defs>
-                                            <clipPath>
-                                                <rect width="24" height="24" fill="white"
-                                                    transform="translate(0.444336 0.366516)"></rect>
-                                            </clipPath>
-                                        </defs>
-                                    </svg>
-                                    <div class="toolbox-text">Tài khoản</div>
-                                </a>
+                                <div class="dropdown">
+                                    <a href="#" class="uk-flex uk-flex-bottom dropdown-toggle">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
+                                            <g>
+                                                <path
+                                                    d="M21.4443 24.3665H19.4443V19.3235C19.4435 18.5395 19.1317 17.7879 18.5774 17.2335C18.023 16.6791 17.2713 16.3673 16.4873 16.3665H8.40134C7.61733 16.3673 6.86567 16.6791 6.3113 17.2335C5.75693 17.7879 5.44513 18.5395 5.44434 19.3235V24.3665H3.44434V19.3235C3.44592 18.0093 3.96869 16.7494 4.89796 15.8201C5.82723 14.8909 7.08714 14.3681 8.40134 14.3665H16.4873C17.8015 14.3681 19.0614 14.8909 19.9907 15.8201C20.92 16.7494 21.4427 18.0093 21.4443 19.3235V24.3665Z"
+                                                    fill="#253D4E"></path>
+                                                <path
+                                                    d="M12.4443 12.3665C11.2577 12.3665 10.0976 12.0146 9.11092 11.3553C8.12422 10.696 7.35519 9.75898 6.90106 8.66262C6.44694 7.56626 6.32812 6.35986 6.55963 5.19598C6.79114 4.03209 7.36258 2.96299 8.2017 2.12388C9.04081 1.28476 10.1099 0.713318 11.2738 0.481807C12.4377 0.250296 13.6441 0.369116 14.7404 0.823242C15.8368 1.27737 16.7739 2.0464 17.4332 3.0331C18.0924 4.01979 18.4443 5.17983 18.4443 6.36652C18.4427 7.95733 17.8101 9.48253 16.6852 10.6074C15.5604 11.7323 14.0352 12.3649 12.4443 12.3665ZM12.4443 2.36652C11.6532 2.36652 10.8799 2.60111 10.2221 3.04064C9.56426 3.48017 9.05157 4.10488 8.74882 4.83579C8.44607 5.56669 8.36686 6.37096 8.5212 7.14688C8.67554 7.9228 9.0565 8.63554 9.61591 9.19495C10.1753 9.75436 10.8881 10.1353 11.664 10.2897C12.4399 10.444 13.2442 10.3648 13.9751 10.062C14.706 9.75929 15.3307 9.2466 15.7702 8.5888C16.2097 7.931 16.4443 7.15764 16.4443 6.36652C16.4443 5.30565 16.0229 4.28824 15.2728 3.53809C14.5226 2.78795 13.5052 2.36652 12.4443 2.36652Z"
+                                                    fill="#253D4E"></path>
+                                            </g>
+                                            <defs>
+                                                <clipPath>
+                                                    <rect width="24" height="24" fill="white" transform="translate(0.444336 0.366516)">
+                                                    </rect>
+                                                </clipPath>
+                                            </defs>
+                                        </svg>
+                                        <div class="toolbox-text">
+                                            {{ Auth::guard('customers')->check() ? Auth::guard('customers')->user()->name : __('info.account') }}
+                                        </div>
+                                    </a>
+                                    <div class="dropdown-content">
+                                        <a href="#">{{ __('info.info') }}</a>
+                                        <a href="{{ route('authClient.login') }}">{{ __('info.setting_account') }}</a>
+                                        <a href="{{ route('authClient.logout') }}">{{ __('info.logout') }}</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
